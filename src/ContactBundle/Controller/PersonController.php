9b51f897081b17e24a7583d6d09d8a16436ddfc4
ptyszer <p.tyszer@hotmail.com>
2018-04-27 11:59:46 +0200

    Add address form

--- src/ContactBundle/Controller/PersonController.php
+++ src/ContactBundle/Controller/PersonController.php
@@ -2,12 +2,17 @@
 
 namespace ContactBundle\Controller;
 
+use ContactBundle\Entity\Address;
+use ContactBundle\Entity\Email;
 use ContactBundle\Entity\Person;
+use ContactBundle\Entity\Phone;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 class PersonController extends Controller
 {
@@ -60,8 +65,11 @@ class PersonController extends Controller
     public function modifyGetAction($id)
     {
         $person = $this->getDoctrine()->getRepository(Person::class)->findOneBy(['id' => $id]);
+        $address = new Address();
+        $email = new Email();
+        $phone = new Phone();
 
-        $form = $this->createFormBuilder($person)
+        $formPerson = $this->createFormBuilder($person)
             ->setMethod('POST')
             ->add('firstName', TextType::class, ['attr' => ['class' => 'form-control']])
             ->add('lastName', TextType::class, ['attr' => ['class' => 'form-control']])
@@ -69,7 +77,21 @@ class PersonController extends Controller
             ->add('save', SubmitType::class, ['label' => 'Edit Person', 'attr' => ['class' => 'btn btn-primary']])
             ->getForm();
 
-        return $this->render('@Contact/Person/modify.html.twig', array('form' => $form->createView()));
+        $formAddress = $this->createFormBuilder($address)
+            ->setAction($this->generateUrl('contact_person_addaddress', ['id' => $person->getId()]))
+            ->setMethod('POST')
+            ->add('city', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('street', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('house', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('flat', NumberType::class, ['attr' => ['class' => 'form-control']])
+            ->add('type', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('save', SubmitType::class, ['label' => 'Add address', 'attr' => ['class' => 'btn btn-primary']])
+            ->getForm();
+
+        return $this->render('@Contact/Person/modify.html.twig', array(
+            'formPerson' => $formPerson->createView(),
+            'formAddress' => $formAddress->createView(),
+        ));
     }
 
     /**
@@ -131,4 +153,34 @@ class PersonController extends Controller
         return $this->render('@Contact/Person/show_all.html.twig', array('persons' => $persons));
     }
 
+    /**
+     * @Route("/{id}/addAddress", methods={"POST"})
+     */
+    public function addAddressAction(Request $request, $id)
+    {
+        $address = new Address();
+        $person = $this->getDoctrine()->getRepository(Person::class)->findOneBy(['id' => $id]);
+
+        $formAddress = $this->createFormBuilder($address)
+            ->setAction($this->generateUrl('contact_person_addaddress', ['id' => $person->getId()]))
+            ->setMethod('POST')
+            ->add('city', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('street', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('house', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('flat', NumberType::class, ['attr' => ['class' => 'form-control']])
+            ->add('type', TextType::class, ['attr' => ['class' => 'form-control']])
+            ->add('save', SubmitType::class, ['label' => 'Add address', 'attr' => ['class' => 'btn btn-primary']])
+            ->getForm();
+
+        $formAddress->handleRequest($request);
+
+        if ($formAddress->isSubmitted() && $formAddress->isValid()) {
+            $address = $formAddress->getData();
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($address->setPerson($person));
+            $em->flush();
+            return $this->redirectToRoute('contact_person_showsingle', ['id' => $person->getId()]);
+        }
+        return new Response('<html><body>Błąd</body></html>');
+    }
 }
